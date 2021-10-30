971a16f3afd702b05acb52a83c0845d0dd049029
Christoffer <links685@gmail.com>
2021-10-30 18:22:04 +0200

    working on ex10 task b

---
+++ exercises/src/main/java/org/pg4200/ex10/PatternExamplesImp.java
@@ -0,0 +1,23 @@
+package org.pg4200.ex10;
+
+public class PatternExamplesImp implements PatternExamples {
+    @Override
+    public String dnaRegex() {
+        return "(C|G|A|T)+";
+    }
+
+    @Override
+    public String telephoneNumberRegex() {
+        return "(((00|\\+)[0-9]{2})?[0-9]{8})";
+    }
+
+    @Override
+    public String emailRegex() {
+        return null;
+    }
+
+    @Override
+    public String isItAJoke() {
+        return null;
+    }
+}
---
+++ exercises/src/main/java/org/pg4200/ex10/TextSearchKMPCached.java
@@ -0,0 +1,8 @@
+package org.pg4200.ex10;
+
+import org.pg4200.les10.search.TextSearchKMP;
+
+public class TextSearchKMPCached extends TextSearchKMP {
+
+
+}
